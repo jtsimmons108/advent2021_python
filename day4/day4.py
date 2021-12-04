23e1a25fbfd534ae5c63bc51d8566307d5f737e6
Jeremiah Simmons <jsimmons@psdr3.org>
2021-12-04 00:05:31 -0600

    rework day 4

--- day4/day4.py
+++ day4/day4.py
@@ -19,29 +19,18 @@ def check_board(board):
             return True
     return False
 
-winning = None
-first = None
+scores = []
 for call in calls:
     for board in boards:
         for row in board:
             if call in row:
                 row[row.index(call)] = 'x'
                 break
-    for board in boards:
+    for i, board in enumerate(boards):
         if check_board(board):
-            if winning == None:
-                winning = board
-                first = int(call)
-            if len(boards) > 1:
-                del boards[boards.index(board)]
-    if len(boards) == 1 and check_board(boards[0]):
-        last = int(call)
-        break
+            score = sum(map(lambda row: sum([int(el) for el in row if el.isdigit()]), board))
+            scores.append(score * int(call))
+            del boards[i]
 
-winning_total = sum(map(lambda row: sum([int(el) for el in row if el.isdigit()]), winning))
-print('Part 1:', winning_total * first)
-
-losing_total = sum(map(lambda row: sum([int(el) for el in row if el.isdigit()]), boards[0]))
-print('Part 2:', losing_total * last)
-
-
+print('Part 1:', scores[0])
+print('Part 2:', scores[-1])
